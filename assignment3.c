a21e1e642dab9f353acb462c6f996066fab79671
“Riz” <rnursaidy@gmail.com>
2023-12-07 11:57:39 -0800

    base code

--- assignment3.c
+++ assignment3.c
@@ -9,115 +9,11 @@ typedef struct Node {
     struct Node* right;
 } Node;
 
-Node* createNode(char* firstName, char* lastName) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    strcpy(newNode->firstName, firstName);
-    strcpy(newNode->lastName, lastName);
-    newNode->left = newNode->right = NULL;
-    return newNode;
-}
-Node* addNode(Node* root, char* firstName, char* lastName) {
-    if (root == NULL) {
-        return createNode(firstName, lastName);
-    }
-
-    int cmp = strcmp(firstName, root->firstName);
-    if (cmp == 0) {
-        cmp = strcmp(lastName, root->lastName);
-        if (cmp == 0) {
-            // Duplicate node, do not add it
-            return root;
-        }
-    }
-
-    if (cmp < 0) {
-        root->left = addNode(root->left, firstName, lastName);
-    } else {
-        root->right = addNode(root->right, firstName, lastName);
-    }
-
-    return root;
-}
-
-
-Node* minValueNode(Node* node) {
-    Node* current = node;
-    while (current && current->left != NULL) {
-        current = current->left;
-    }
-    return current;
-}
-Node* deleteNode(Node* root, char* firstName, char* lastName, FILE* output) {
-    if (root == NULL) {
-        fprintf(output, "Not Found\n");
-        return root; // Return root without adding a node
-    }
-
-    int cmp = strcmp(firstName, root->firstName);
-    if (cmp == 0) {
-        cmp = strcmp(lastName, root->lastName);
-    }
-
-    if (cmp < 0) {
-        root->left = deleteNode(root->left, firstName, lastName, output);
-    } else if (cmp > 0) {
-        root->right = deleteNode(root->right, firstName, lastName, output);
-    } else {
-        fprintf(output, "Found\n");
-        // Node with only one child or no child
-        if (root->left == NULL) {
-            Node* temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            Node* temp = root->left;
-            free(root);
-            return temp;
-        }
-
-        // Node with two children: Get the inorder successor
-        Node* temp = minValueNode(root->right);
-
-        // Copy the inorder successor's content to this node
-        strcpy(root->firstName, temp->firstName);
-        strcpy(root->lastName, temp->lastName);
-
-        // Delete the inorder successor
-        root->right = deleteNode(root->right, temp->firstName, temp->lastName, output);
-    }
-
-    return root;
-}
-
-
-int searchNode(Node* root, char* firstName, char* lastName, FILE* output) {
-    if (root == NULL) {
-        fprintf(output, "Not Found\n");
-        return 0;
-    }
-
-    int cmp = strcmp(firstName, root->firstName);
-    if (cmp == 0) {
-        cmp = strcmp(lastName, root->lastName);
-    }
-
-    if (cmp < 0) {
-        return searchNode(root->left, firstName, lastName, output);
-    } else if (cmp > 0) {
-        return searchNode(root->right, firstName, lastName, output);
-    } else {
-        fprintf(output, "Found\n");
-        return 1;
-    }
-}
-
-void preOrderTraversal(Node* root, FILE* output) {
-    if (root != NULL) {
-        fprintf(output, "%s %s\n", root->firstName, root->lastName);
-        preOrderTraversal(root->left, output);
-        preOrderTraversal(root->right, output);
-    }
-}
+Node* createNode(char* firstName, char* lastName);
+Node* addNode(Node* root, char* firstName, char* lastName);
+Node* deleteNode(Node* root, char* firstName, char* lastName);
+Node* searchNode(Node* root, char* firstName, char* lastName);
+void preOrder(Node* root, FILE* outputFile);
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -126,46 +22,138 @@ int main(int argc, char* argv[]) {
     }
 
     FILE* inputFile = fopen(argv[1], "r");
-    if (inputFile == NULL) {
-        printf("Error: Unable to open input file\n");
-        return 1;
-    }
-
     FILE* outputFile = fopen(argv[2], "w");
-    if (outputFile == NULL) {
-        printf("Error: Unable to open output file\n");
+
+    if (!inputFile || !outputFile) {
+        printf("Error: Unable to open file\n");
         return 1;
     }
 
     Node* root = NULL;
-    char firstName[50];
-    char lastName[50];
+    char firstName[50], lastName[50];
     int operation;
 
-    while (fscanf(inputFile, "%s %s %d", firstName, lastName, &operation) != EOF) {
+    char line[256];
+    while (fgets(line, sizeof(line), inputFile)) {
+    if (sscanf(line, "%s %s %d", firstName, lastName, &operation) == 3) {
         switch (operation) {
             case 1:
                 root = addNode(root, firstName, lastName);
                 break;
             case 2:
-                root = deleteNode(root, firstName, lastName, outputFile);
+                if (searchNode(root, firstName, lastName)) {
+                    root = deleteNode(root, firstName, lastName);
+                }
                 break;
             case 3:
-                searchNode(root, firstName, lastName, outputFile);
-                break;
-            case 4:
-                preOrderTraversal(root, outputFile);
-                fprintf(outputFile, "\n");
+                if (searchNode(root, firstName, lastName)) {
+                    fprintf(outputFile, "Found\n");
+                } else {
+                    fprintf(outputFile, "Not Found\n");
+                }
                 break;
             default:
                 printf("Error: Invalid operation\n");
                 return 1;
         }
+    } else if (sscanf(line, "%d", &operation) == 1 && operation == 4) {
+        preOrder(root, outputFile);
+    } else {
+        printf("Error: Invalid input\n");
+        return 1;
     }
-
+}
 
     fclose(inputFile);
     fclose(outputFile);
 
     return 0;
+}
+
+// Implement the createNode, addNode, deleteNode, searchNode, and preOrder functions here
+
+Node* createNode(char* firstName, char* lastName) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    strcpy(newNode->firstName, firstName);
+    strcpy(newNode->lastName, lastName);
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* addNode(Node* root, char* firstName, char* lastName) {
+    if (root == NULL) {
+        return createNode(firstName, lastName);
+    }
+
+    if (strcmp(lastName, root->lastName) < 0) {
+        root->left = addNode(root->left, firstName, lastName);
+    } else if (strcmp(lastName, root->lastName) > 0) {
+        root->right = addNode(root->right, firstName, lastName);
+    } else {
+        if (strcmp(firstName, root->firstName) < 0) {
+            root->left = addNode(root->left, firstName, lastName);
+        } else {
+            root->right = addNode(root->right, firstName, lastName);
+        }
+    }
+
+    return root;
+}
+
+Node* minValueNode(Node* node) {
+    Node* current = node;
+    while (current && current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+Node* deleteNode(Node* root, char* firstName, char* lastName) {
+    if (root == NULL) return root;
+    if (strcmp(lastName, root->lastName) < 0)
+        root->left = deleteNode(root->left, firstName, lastName);
+    else if (strcmp(lastName, root->lastName) > 0)
+        root->right = deleteNode(root->right, firstName, lastName);
+    else {
+        if (strcmp(firstName, root->firstName) < 0)
+            root->left = deleteNode(root->left, firstName, lastName);
+        else if (strcmp(firstName, root->firstName) > 0)
+            root->right = deleteNode(root->right, firstName, lastName);
+        else {
+            if (root->left == NULL) {
+                Node* temp = root->right;
+                free(root);
+                return temp;
+            } else if (root->right == NULL) {
+                Node* temp = root->left;
+                free(root);
+                return temp;
+            }
+            Node* temp = minValueNode(root->right);
+            strcpy(root->firstName, temp->firstName);
+            strcpy(root->lastName, temp->lastName);
+            root->right = deleteNode(root->right, temp->firstName, temp->lastName);
+        }
+    }
+    return root;
+}
+
+Node* searchNode(Node* root, char* firstName, char* lastName) {
+    if (root == NULL || (strcmp(firstName, root->firstName) == 0 && strcmp(lastName, root->lastName) == 0))
+        return root;
+    if (strcmp(lastName, root->lastName) < 0)
+        return searchNode(root->left, firstName, lastName);
+    if (strcmp(lastName, root->lastName) > 0)
+        return searchNode(root->right, firstName, lastName);
+    if (strcmp(firstName, root->firstName) < 0)
+        return searchNode(root->left, firstName, lastName);
+    return searchNode(root->right, firstName, lastName);
+}
+
+void preOrder(Node* root, FILE* outputFile) {
+    if (root != NULL) {
+        preOrder(root->left, outputFile);
+        fprintf(outputFile, "%s %s\n", root->firstName, root->lastName);
+        preOrder(root->right, outputFile);
+    }
 }
